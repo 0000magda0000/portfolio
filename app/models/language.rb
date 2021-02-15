53667efa3d3590d84e0d856a3b8636b118a01cc9
Magda Barthofer <magda.barthofer@gmail.com>
2021-02-15 20:03:43 +0100

    fixed seeds added portfolio

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language < ApplicationRecord
+  has_many :projects
   has_many :skills
   has_many :projects, through: :skills
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,12 +24,11 @@ puts "Created Project"
 queermed_languages_url = 'https://api.github.com/repos/clara-lupa/queermed/languages'
 queermed_languages_serialized = open(queermed_languages_url).read
 queermed_languages = JSON.parse(queermed_languages_serialized)
-queermed_languages_infos = []
+queermed_languages_array = []
 queermed_languages.each do |language, statistics|
-  queermed_languages_infos << Language.create(name: language, statistics: statistics)
+  queermed_languages_array << Language.create(name: language, statistics: statistics)
 end
 
-queermed_languages_infos_count = queermed_languages_infos.count
 puts "Created Languages with Statistics"
 
 queermed_contributers_url = 'https://api.github.com/repos/clara-lupa/queermed/contributors'
@@ -37,27 +36,26 @@ queermed_contributers_url_serialized = open(queermed_contributers_url).read
 queermed_contributers_parse = JSON.parse(queermed_contributers_url_serialized)
 queermed_contributer_array = []
 
-
 queermed_contributers_parse.each do |contributer, _value|
-    Contributer.create!(
-      name: contributer['login']
-    )
-end
-queermed_contributors_count = queermed_contributers_parse.count
-puts "Created Contributers"
-
-  Contributer.all[0..queermed_contributors_count - 1].each do |contributer|
-    Info.create!(
-      project_id: queermed_project.id,
-      contributer_id: contributer.id
+  queermed_contributer_array << Contributer.create!(
+   name: contributer['login']
   )
 end
+puts "Created Contributers"
+
+queermed_contributer_array.each do |qca|
+  Info.create(
+    project_id: queermed_project.id,
+    contributer_id: qca.id
+  )
+end
+
 puts "Created Infos"
 
-Language.all[0..queermed_languages_infos_count - 1].each do |language|
-  Skill.create!(
+queermed_languages_array.each do |qla|
+  Skill.create(
     project_id: queermed_project.id,
-    language_id: language.id
+    language_id: qla.id
   )
 end
 
@@ -78,9 +76,9 @@ puts "Created Project"
 instrumentalize_languages_url = 'https://api.github.com/repos/clara-lupa/instrumentalize/languages'
 instrumentalize_languages_serialized = open(instrumentalize_languages_url).read
 instrumentalize_languages = JSON.parse(instrumentalize_languages_serialized)
-instrumentalize_languages_infos = []
+instrumentalize_languages_array = []
 instrumentalize_languages.each do |language, statistics|
-  instrumentalize_languages_infos << Language.create(name: language, statistics: statistics)
+  instrumentalize_languages_array << Language.create(name: language, statistics: statistics)
 end
 puts "Created Languages with Statistics"
 
@@ -88,28 +86,85 @@ instrumentalize_contributers_url = 'https://api.github.com/repos/clara-lupa/inst
 instrumentalize_contributers_url_serialized = open(instrumentalize_contributers_url).read
 instrumentalize_contributers_parse = JSON.parse(instrumentalize_contributers_url_serialized)
 instrumentalize_contributers_parse_count = instrumentalize_contributers_parse.count
+instrumentalize_contributer_array = []
 
 instrumentalize_contributers_parse.each do |contributer, _value|
-  Contributer.create!(
+  instrumentalize_contributer_array << Contributer.create!(
     name: contributer['login']
   )
 end
 puts "Created Contributers"
 
-Contributer.all[queermed_contributors_count.. - 1].each do |contributer|
-  Info.create!(
+instrumentalize_contributer_array.each do |ica|
+  Info.create(
     project_id: instrumentalize_project.id,
-    contributer_id: contributer.id
+    contributer_id: ica.id
+  )
+end
+
+puts "Created Infos"
+
+instrumentalize_languages_array.each do |ila|
+  Skill.create(
+    project_id: instrumentalize_project.id,
+    language_id: ila.id
+  )
+end
+
+
+puts "Created Skills"
+puts "CREATED INSTRUMENTALIZE"
+
+portfolio_url = 'https://api.github.com/repos/0000magda0000/portfolio'
+portfolio_serialized = open(portfolio_url).read
+portfolio = JSON.parse(portfolio_serialized)
+
+portfolio_project = Project.create!(
+  name: portfolio['name'],
+  first_push: portfolio['created_at'],
+  github_link: portfolio['html_url'],
+  website: 'https://www.portfolio.info')
+puts "Created Project"
+
+portfolio_languages_url = 'https://api.github.com/repos/0000magda0000/portfolio/languages'
+portfolio_languages_serialized = open(portfolio_languages_url).read
+portfolio_languages = JSON.parse(portfolio_languages_serialized)
+portfolio_languages_array = []
+portfolio_languages.each do |language, statistics|
+  portfolio_languages_array << Language.create(name: language, statistics: statistics)
+end
+
+puts "Created Languages with Statistics"
+
+portfolio_contributers_url = 'https://api.github.com/repos/0000magda0000/portfolio/contributors'
+portfolio_contributers_url_serialized = open(portfolio_contributers_url).read
+portfolio_contributers_parse = JSON.parse(portfolio_contributers_url_serialized)
+portfolio_contributer_array = []
+
+
+portfolio_contributers_parse.each do |contributer, _value|
+portfolio_contributer_array <<  Contributer.create!(
+      name: contributer['login']
+    )
+end
+portfolio_contributors_count = portfolio_contributers_parse.count
+puts "Created Contributers"
+
+portfolio_contributer_array.each do |pca|
+  Info.create(
+    project_id: portfolio_project.id,
+    contributer_id: pca.id
   )
 end
 puts "Created Infos"
 
-Language.all[queermed_languages_infos_count.. - 1].each do |language|
-  Skill.create!(
-    project_id: instrumentalize_project.id,
-    language_id: language.id
+portfolio_languages_array.each do |pla|
+  Skill.create(
+    project_id: portfolio_project.id,
+    language_id: pla.id
   )
 end
 
 puts "Created Skills"
-puts "CREATED instrumentalize"
+puts "CREATED PORTFOLIO"
+
