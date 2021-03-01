6064fc10c664c9cb65a8dcdaf91c225e41eb46f0
Magda Barthofer <magda.barthofer@gmail.com>
2021-03-01 13:30:59 +0100

    changed seeds and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -336,3 +336,23 @@ bitsycontributers = Contributer.create!(
 Skill.create!(language_id: bitsylanguages.id, project_id: bitsy.id)
 Info.create!(contributer_id: bitsycontributers.id, project_id: bitsy.id)
 puts "CREATED BITSYKNOX"
+
+
+peterhanson_project = Project.create!(
+  name: "peterhansonmusic.com",
+  website: "http://peterhansonmusic.com",
+  first_push: "26th Feb 2021".to_date
+  )
+peterhanson_project.photo.attach(io: File.open('app/assets/images/peterhanson.png'), filename: 'peterhanson.png', content_type: 'image/png')
+
+
+peterhanson_languages = Language.create!(name: "WordPress")
+
+peterhanson_contributers = Contributer.create!(
+  name: "0000magda0000",
+  github: "https://github.com/0000magda0000"
+  )
+
+Skill.create!(language_id: peterhanson_languages.id, project_id: peterhanson_project.id)
+Info.create!(contributer_id: peterhanson_contributers.id, project_id: peterhanson_project.id)
+puts "CREATED PETERHANSONMUSIC"
