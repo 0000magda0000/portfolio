3b9be9d779b4665aa4c8c267644083c6a3e55c46
Magda Barthofer <magda.barthofer@gmail.com>
2021-04-02 14:58:27 +0200

    added pid file to gitignore

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,19 @@
-upstream app {
-    # Path to Unicorn SOCK file, as defined previously
-    server unix:/home/deploy/portfolio/shared/sockets/unicorn.sock fail_timeout=0;
-}
-server {
-    listen 80;
-    server_name localhost;
-root /home/deploy/appname/public;
-try_files $uri/index.html $uri @app;
-location @app {
-        proxy_pass http://app;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $http_host;
-        proxy_redirect off;
-    }
-error_page 500 502 503 504 /500.html;
-    client_max_body_size 4G;
-    keepalive_timeout 10;
-}
+# set path to the application
+app_dir = File.expand_path("../../../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+working_directory "#{app_dir}/current"
+
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 30
+
+# Path for the Unicorn socket
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Set path for logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set proccess id path
+pid "#{shared_dir}/unicorn.pid"
